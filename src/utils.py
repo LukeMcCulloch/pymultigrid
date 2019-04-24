74906e5a56529f52cdd237f73839ce87d65102d4
Luke McCulloch <tlukemcculloch@gmail.com>
2019-04-23 22:50:04 -0500

    initial commit of prolongation, restriction, and smoothing

---
+++ src/utils.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 17:22:44 2019
+
+@author: Luke.McCulloch
+"""
+import numpy as np
+
+def l2_norm(a,h):
+    return h*np.sqrt(np.sum(a**2))
+
+def prolong_lin(a):
+    pshape = (2*np.shape(a)[0]-1,
+              2*np.shape(a)[1]-1)
+    p = np.empty(pshape,float)
+    p[0:    :2,0:       :2]    =   a[0:,    0:]
+    p[1:    -1:2,0:     :2]     =   0.5*(a[0:-1, 0:  ] + a[1:  ,0: ]  )
+    p[0:    :2,1:     -1:2]     =   0.5*(a[0: , 0: -1 ] + a[0:  ,1: ]  )
+    p[1:-1    :2,1:   -1:2]     =   0.25*(a[0:-1, 0: -1 ] + a[1:  ,0:-1 ] +
+                                         a[0:-1,1:] + a[1:,1:])
+    return p
+    
+def restrict_hw(a):
+    rshape = (np.shape(a)[0]/2 + 1,
+              np.shape(a)[1]/2 + 1)
+    r = np.empty(rshape,float)
+    r[1:-1,1:-1] = 0.5*a[2:-1:2,2:-1:2] + \
+                    0.125*(a[2:-1:2,1:-2:2]+a[2:-1:2,3:  :2]+
+                           a[1:-2:2, 2:-1:2]+a[3:    :2,2:-1:2])
+    r[0,    0:] = a[0,  0:  :2]
+    r[-1,   0:] = a[-1, 0:  :2]
+    r[0:,    0] = a[0:  :2,  0]
+    r[0:,   -1] = a[0:  :2, -1]
+    return r
+
+
+if __name__ == """__main__""":
+    a = np.linspace(1,81,81)
+    b = a.reshape(9,9)
+    c = restrict_hw(b)
+    d = prolong_lin(c)
+    
+    
+    print 'init grid: b='
+    print b,'\n'
+    print 'with sp[acing 1 its norm is ',l2_norm(b,1),'\n'
+    print 'restrict grid -> '
+    print c,'\n'
+    print 'prolong grid -> '
+    print d
