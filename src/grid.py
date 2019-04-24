46af459dd42e73e5ef88d552aa40537755596a24
Luke McCulloch <Luke.McCulloch@bentley.com>
2019-04-24 11:02:32 -0500

    multigrid in python

---
+++ src/grid.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 10:48:17 2019
+
+@author: Luke.McCulloch
+"""
+# File: grid.py: linked grid structures and associated algorithms.
+
+import numpy as np
+from utils import l2_norm, restrict_hw, prolong_lin
+from smooth import gs_rb_step, get_lhs, solve
+
+class Grid:
+    """
+    A Grid contains the structures and algorithms for a
+    given level, together with a pointer to a coarser grid.
+    """
+    def __init__(self,name,steplength,u,f,coarser=None):
+        self.name=name
+        self.co=coarser # pointer to a coarser grid
+        self.h=steplength # step length h
+        self.h2=steplength**2 # h**2
+        self.u=u # improved variable array
+        self.f=f # right hand side array
+
+    
+    def __str__(self):
+        """ Generate an information string about this level. """
+        sme='Grid at %s with steplength = %0.4g\n' % (self.name,self.h)
+        if self.co:
+            sco='Coarser grid with name %s\n' % self.co.name
+        else:
+            sco='No coarser grid\n'
+        return sme+sco
+    
+    def smooth(self,nu):
+        """
+        Carry out Newton-Raphson/Gauss-Seidel red-black
+        iteration u --> u, nu times.
+        """
+        print 'Relax in %s for %d times' % (self.name,nu)
+        v=self.u.copy()
+        for i in range(nu):
+            v=gs_rb_step(v,self.f,self.h2)
+        self.u=v
+        
+    def fas_v_cycle(self,nu1,nu2):
+        """ Recursive implementation of (nu1,nu2) FAS V-cycle. """
+        print 'FAS-V-cycle called for grid at %s\n' % self.name
+        # Initial smoothing
+        self.smooth(nu1)
+        if self.co:
+            # There is a coarser grid
+            self.co.u=restrict_hw(self.u)
+            # Get residual
+            res=self.f-get_lhs(self.u,self.h2)
+            # Get coarser f
+            self.co.f=restrict_hw(res)+get_lhs(self.co.u,self.co.h2)
+            oldc=self.co.u
+            # Get new coarse solution
+            newc=self.co.fas_v_cycle(nu1,nu2)
+            # Correct current u
+            self.u+=prolong_lin(newc-oldc)
+        self.smooth(nu2)
+        return self.u
+    
+    def fmg_fas_v_cycle(self,nu0,nu1,nu2):
+        """ Recursive implementation of FMG-FAS_V-cycle. """
+        print 'FMG-FAS-V-cycle called for grid at %s\n' % self.name
+        
+        if not self.co:
+            # Coarsest grid
+            self.u=solve(self.f)
+        else:
+            # Restrict f
+            self.co.f=restrict_hw(self.f)
+            # Use recursion to get coarser u
+            self.co.u=self.co.fmg_fas_v_cycle(nu0,nu1,nu2)
+            # Prolong to current u
+            self.u=prolong_lin(self.co.u)
+        for it in range(nu0):
+            self.u=self.fas_v_cycle(nu1,nu2)
+            return self.u
---
+++ src/rungrid.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 10:51:20 2019
+
+@author: Luke.McCulloch
+"""
+
+# File rungrid.py: runs the multigrid programme.
+import numpy as np
+from grid import Grid
+from smooth import get_lhs
+
+
+n_grids=5
+size_init=2**n_grids+1
+size=size_init-1
+h=1.0/size
+foo=[] # Container list for grids
+
+
+for k in range(n_grids):
+    # Set up list of grids
+    u=np.zeros((size+1,size+1),float)
+    f=np.zeros_like(u)
+    name='Level '+str(n_grids-1-k)
+    temp=Grid(name,h,u,f)
+    foo.append(temp)
+    size/=2
+    h*=2
+    
+    
+for k in range(1,n_grids):
+    # Set up coarser Grid links
+    foo[k-1].co=foo[k]
+    
+    
+    
+# Check that the initial construction works
+for k in range(n_grids):
+    print foo[k]
+    
+# Set up data for the Numerical Recipes problem
+u_init=np.zeros((size_init,size_init))
+f_init=np.zeros_like(u_init)
+f_init[size_init/2,size_init/2]=2.0
+foo[0].u=u_init
+foo[0].f=f_init
+foo[0].fmg_fas_v_cycle(1,1,1)
+
+# As a check, get lhs for the final grid
+lhs=get_lhs(foo[0].u,foo[0].h2)
+print "max abs lhs = ", np.max(np.abs(lhs))
+
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+#%matplotlib notebook
+
+
+
+plt.ion()
+fig1=plt.figure()
+ax1=Axes3D(fig1)
+
+xx,yy=np.mgrid[0:1:1j*size_init,0:1:1j*size_init]
+ax1.plot_surface(xx,yy,1000*foo[0].u,rstride=1,cstride=1,alpha=0.2)
+ax1.set_xlabel('x',style='italic')
+ax1.set_ylabel('y',style='italic')
+ax1.set_zlabel('1000*u',style='italic')
+
+fig2=plt.figure()
+ax2=Axes3D(fig2)
+ax2.plot_surface(xx,yy,lhs,rstride=1,cstride=1,alpha=0.2)
+ax1.set_xlabel('x',style='italic')
+ax2.set_ylabel('y',style='italic')
+ax2.set_zlabel('lhs',style='italic')
+
